f46c5bb3e48ee02c78bc88e040b5e575462ca763
Bronts <bronteda@gmail.com>
2025-09-08 13:32:29 +0100

    added 404 route

--- src/App.jsx
+++ src/App.jsx
@@ -249,6 +249,7 @@ const App = () => {
             <Route path="/sign-in" element={<SignInForm />} />
           </>
         )}
+      <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
     </>
   );
